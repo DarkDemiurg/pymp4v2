db9b319213c33af06d64565a545191e12c69954a
Dmitriy Efimov <efimov@qtech.ru>
2025-09-03 15:36:12 +0300

    Test script

---
+++ test_pymp4v2.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+sys.path.insert(0, './build')
+
+import pymp4v2
+
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+else:
+    print("No filename provided as argument.")
+
+mp4_file = pymp4v2.MP4File(name)
+
+print("Info:", mp4_file.get_info())
